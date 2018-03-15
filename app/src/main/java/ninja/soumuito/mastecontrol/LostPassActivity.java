a3af90a50564b9c76c071ced879e55cb32aabd33
André Cardoso <anbc@outlook.com>
2018-03-15 20:28:20 -0300

    Lost Pass Add

--- app/src/main/java/ninja/soumuito/mastecontrol/LostPassActivity.java
+++ app/src/main/java/ninja/soumuito/mastecontrol/LostPassActivity.java
@@ -1,19 +1,64 @@
 package ninja.soumuito.mastecontrol;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.view.WindowManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LostPassActivity extends AppCompatActivity {
+
+    private EditText inputEmail;
+    private Button reset_buton;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_lost_pass);
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        reset_buton = (Button) findViewById(R.id.reset_buton);
+
+
+
+        auth = FirebaseAuth.getInstance();
+
+        reset_buton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                auth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LostPassActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(LostPassActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
+
+
+                            }
+                        });
+            }
+        });
     }
+
 }
