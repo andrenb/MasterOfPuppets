7b9ed8d6f5f5f6493388ef20bce613476723424d
André Cardoso <anbc@outlook.com>
2018-03-15 20:38:35 -0300

    New Account add

--- app/src/main/java/ninja/soumuito/mastecontrol/SingUpActivity.java
+++ app/src/main/java/ninja/soumuito/mastecontrol/SingUpActivity.java
@@ -1,101 +1,83 @@
 package ninja.soumuito.mastecontrol;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.UUID;
-
 public class SingUpActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    private EditText inputEmail, inputPassword;
+    private Button  singUp;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_sing_up);
+              auth = FirebaseAuth.getInstance();
 
+        singUp = (Button) findViewById(R.id.singUp);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.text_pass);
 
-
-        mAuth = FirebaseAuth.getInstance();
-
-
-    Button singUp  = findViewById(R.id.singUp);
         singUp.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            wireteTest();
+            @Override
+            public void onClick(View v) {
 
-        }
-    });
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
 
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SingUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SingUpActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(SingUpActivity.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(SingUpActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
-public void wireteTest() {
-
-        EditText name_text = findViewById(R.id.name_text);
-
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference novoaluno = database.getReference("iesb/alunos/"+ UUID.randomUUID().toString());
 
 
-    novoaluno.child("nome").setValue(name_text.getText().toString());
-    novoaluno.child("matricula").setValue("1234124");
-    novoaluno.child("campus").setValue("sul");
-    novoaluno.child("dt_nasc").setValue("1234412341");
-    novoaluno.child("curso").setValue("Culinaria");
-    novoaluno.child("interece").child("it1").setValue("computa");
-    novoaluno.child("interece").child("it2").setValue("ingles");
-
-    }
-
-    public void createUser() {
-
-        EditText text_pass = findViewById(R.id.text_pass);
-        EditText mailText = findViewById(R.id.mailText);
-
-
-
-
-        mAuth.signInWithEmailAndPassword(mailText.getText().toString(),text_pass.getText().toString())
-                .addOnCompleteListener(SingUpActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-
-                        if (task.isSuccessful()) {
-                            Log.d("AUTH", "signInWithEmail:success");
-
-                            Intent intent = new Intent(SingUpActivity.this, MainActivity.class);
-                            startActivity(intent);
-                        }
-                        else {
-                            Log.w("AUTH", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(SingUpActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                        // ...
-                    }
-                });
-
-
-    }
 }
